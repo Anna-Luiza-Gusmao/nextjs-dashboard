b5968f67e9fa5b5a5ceeb8921ef7df0add0e3ac3
Anna Luiza Gusmão <117766753+anna-luiza-haus@users.noreply.github.com>
2024-07-11 11:38:38 -0300

    feat: authentication with email and password

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import NextAuth from 'next-auth'
+import { authConfig } from './auth.config'
+
+export default NextAuth(authConfig).auth
+
+// A vantagem de empregar o Middleware para essa tarefa é que as rotas protegidas não começarão a renderizar até que o Middleware verifique a autenticação,
+// melhorando a segurança e o desempenho do seu aplicativo.
+
+export const config = {
+    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
