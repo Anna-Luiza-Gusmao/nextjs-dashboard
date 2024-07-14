01a60a346724749cace7fe79018d59e50a92d73d
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2024-07-13 23:51:33 -0300

    chore: correct skeletons for the customer page

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -96,7 +96,7 @@ export default function DashboardSkeleton() {
 	)
 }
 
-export function TableRowSkeleton() {
+export function InvoicesTableRowSkeleton() {
 	return (
 		<tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
 			{/* Customer Name and Image */}
@@ -194,15 +194,79 @@ export function InvoicesTableSkeleton() {
 							</tr>
 						</thead>
 						<tbody className="bg-white">
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
+							<InvoicesTableRowSkeleton />
+							<InvoicesTableRowSkeleton />
+							<InvoicesTableRowSkeleton />
+							<InvoicesTableRowSkeleton />
+							<InvoicesTableRowSkeleton />
+							<InvoicesTableRowSkeleton />
 						</tbody>
 					</table>
 				</div>
+			</div>
+		</div>
+	)
+}
+
+export function CustomersTableRowSkeleton() {
+	return (
+		<tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+			{/* Customer Name and Image */}
+			<td className="relative overflow-hidden whitespace-nowrap py-3 pl-6 pr-3">
+				<div className="flex items-center gap-3">
+					<div className="h-8 w-8 rounded-full bg-gray-100"></div>
+					<div className="h-6 w-24 rounded bg-gray-100"></div>
+				</div>
+			</td>
+			{/* Email */}
+			<td className="whitespace-nowrap px-3 py-3">
+				<div className="h-6 w-32 rounded bg-gray-100"></div>
+			</td>
+			{/* Total invoices */}
+			<td className="whitespace-nowrap px-3 py-3">
+				<div className="h-6 w-16 rounded bg-gray-100"></div>
+			</td>
+			{/* Pending invoices */}
+			<td className="whitespace-nowrap px-3 py-3">
+				<div className="h-6 w-24 rounded bg-gray-100"></div>
+			</td>
+			{/* Paid invoices */}
+			<td className="whitespace-nowrap px-3 py-3">
+				<div className="h-6 w-24 rounded bg-gray-100"></div>
+			</td>
+			{/* Actions */}
+			{/* <td className="whitespace-nowrap py-3 pl-6 pr-3"> */}
+				{/* <div className="flex justify-end gap-3"> */}
+					{/* <div className="h-[38px] w-[38px] rounded bg-gray-100"></div> */}
+					{/* <div className="h-[38px] w-[38px] rounded bg-gray-100"></div> */}
+				{/* </div> */}
+			{/* </td> */}
+		</tr>
+	)
+}
+
+export function CustomersMobileSkeleton() {
+	return (
+		<div className="mb-2 w-full rounded-md bg-white p-4">
+			<div className="flex flex-col gap-2 border-b border-gray-100 pb-4">
+				<div className="flex items-center">
+					<div className="mr-2 h-8 w-8 rounded-full bg-gray-100"></div>
+					<div className="h-6 w-32 rounded bg-gray-100"></div>
+				</div>
+				<div className="h-6 w-28 rounded bg-gray-100"></div>
+			</div>
+			<div className="flex w-full items-center justify-between pt-4 border-b border-gray-100 pb-4">
+				<div>
+					<div className="h-6 w-16 rounded bg-gray-100"></div>
+					<div className="mt-2 h-6 w-28 rounded bg-gray-100"></div>
+				</div>
+				<div>
+					<div className="h-6 w-16 rounded bg-gray-100"></div>
+					<div className="mt-2 h-6 w-28 rounded bg-gray-100"></div>
+				</div>
+			</div>
+			<div className="flex w-full items-center justify-between pt-4">
+				<div className="h-6 w-24 rounded bg-gray-100"></div>
 			</div>
 		</div>
 	)
@@ -214,11 +278,11 @@ export function CustomersTableSkeleton() {
 			<div className="inline-block min-w-full align-middle">
 				<div className="rounded-lg bg-gray-50 p-2 md:pt-0">
 					<div className="md:hidden">
-						<InvoicesMobileSkeleton />
-						<InvoicesMobileSkeleton />
-						<InvoicesMobileSkeleton />
-						<InvoicesMobileSkeleton />
-						<InvoicesMobileSkeleton />
+						<CustomersMobileSkeleton />
+						<CustomersMobileSkeleton />
+						<CustomersMobileSkeleton />
+						<CustomersMobileSkeleton />
+						<CustomersMobileSkeleton />
 					</div>
 					<table className="hidden min-w-full text-gray-900 md:table">
 						<thead className="rounded-lg text-left text-sm font-normal">
@@ -241,11 +305,12 @@ export function CustomersTableSkeleton() {
 							</tr>
 						</thead>
 						<tbody className="bg-white">
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
-							<TableRowSkeleton />
+							<CustomersTableRowSkeleton />
+							<CustomersTableRowSkeleton />
+							<CustomersTableRowSkeleton />
+							<CustomersTableRowSkeleton />
+							<CustomersTableRowSkeleton />
+							<CustomersTableRowSkeleton />
 						</tbody>
 					</table>
 				</div>
