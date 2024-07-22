6c085ecf9741d360d2d51abc474f02cc8fdb00d9
Anna Luiza Gusmão <117766753+anna-luiza-haus@users.noreply.github.com>
2024-07-22 13:30:02 -0300

    fix: metadata title to customers page

--- app/dashboard/clientes/page.tsx
+++ app/dashboard/clientes/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from "next"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-	title: "Customers"
+	title: "Clientes"
 }
 
 export default async function Page({
