ef75311f604846b53c489620e7af8203d46fbef1
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2024-08-03 22:50:21 -0300

    feat: user identification on screen with real data

--- app/ui/user-identification.tsx
+++ app/ui/user-identification.tsx
@@ -1,12 +1,42 @@
-export default function UserIdentification() {
-    return (
-        <section className="flex items-center gap-4">
+import { auth } from "@/auth/auth"
+
+export default async function UserIdentification() {
+    const session = await auth()
+    const userName = session?.user.name || ""
+
+    const userPermission = () => {
+        switch (session?.user.permission) {
+            case "admin":
+                return "Administrador"
+            case "supervisor":
+                return "Supervisor de Cobrança"
+            case "manager":
+                return "Gerente Financeiro"
+            default:
+                return "Contador"
+        }
+    }
+
+    const userInitials = (userName: string) => {
+        const userInitials = userName
+            .split(' ')
+            .map((part: string) => part[0])
+            .join('')
+            .toUpperCase()
+
+        return (
             <div className="flex items-center justify-center w-12 h-12 bg-violet-400 rounded-full text-white text-xl font-medium">
-                <span>JS</span>
+                <span>{userInitials}</span>
             </div>
+        )
+    }
+
+    return (
+        <section className="flex items-center gap-4 min-w-40">
+            {userInitials(userName)}
             <div>
-                <p>Nome do Usuário</p>
-                <p className="text-sm text-gray-500">Função</p>
+                <p>{userName}</p>
+                <p className="text-sm text-gray-500">{userPermission()}</p>
             </div>
         </section>
     )
